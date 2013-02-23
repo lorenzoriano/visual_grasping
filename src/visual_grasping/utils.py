82f6cd297f103b89c2d773cd7686329ad0a41ee8
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-02-22 22:01:39 -0800

    removed support for euclid.py, using tf.transform

--- src/visual_grasping/utils.py
+++ src/visual_grasping/utils.py
@@ -1,48 +1,108 @@
-import euclid
 import copy
 import numpy as np
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import PoseStamped, Pose
+from sensor_msgs.msg import PointCloud2, PointField
 from tf import transformations
 
-def matrix4ToPose(matrix):
-    """Converts a Matrix 4 to a Pose message by extracting the translation and
+def pointcloud2_to_array(cloud_msg):
+    ''' 
+    Converts a rospy PointCloud2 message to a numpy recordarray 
+    
+    Assumes all fields 32 bit floats, and there is no padding.
+    '''
+    dtype_list = [(f.name, np.float32) for f in cloud_msg.fields]
+    cloud_arr = np.fromstring(cloud_msg.data, dtype_list)
+    return np.reshape(cloud_arr, (cloud_msg.height, cloud_msg.width))
+
+def get_xyz_points(cloud_array, remove_nans=True):
+    '''
+    Pulls out x, y, and z columns from the cloud recordarray, and returns a 3xN matrix.
+    '''
+    # remove crap points
+    if remove_nans:
+        mask = np.isfinite(cloud_array['x']) & np.isfinite(cloud_array['y']) & np.isfinite(cloud_array['z'])
+        cloud_array = cloud_array[mask]
+    
+    # pull out x, y, and z values
+    points = np.zeros(list(cloud_array.shape) + [3], dtype=np.float)
+    points[...,0] = cloud_array['x']
+    points[...,1] = cloud_array['y']
+    points[...,2] = cloud_array['z']
+
+    return points
+
+def pointcloud2_to_xyz_array(cloud_msg, remove_nans=True):
+    return get_xyz_points(pointcloud2_to_array(cloud_msg), remove_nans=remove_nans)
+
+def xyz_array_to_pointcloud2(points, stamp=None, frame_id=None):
+    '''
+    Create a sensor_msgs.PointCloud2 from an array
+    of points.
+    '''
+    msg = PointCloud2()
+    if stamp:
+        msg.header.stamp = stamp
+    if frame_id:
+        msg.header.frame_id = frame_id
+    if len(points.shape) == 3:
+        msg.height = points.shape[1]
+        msg.width = points.shape[0]
+    else:
+        msg.height = 1
+        msg.width = len(points)
+    msg.fields = [
+        PointField('x', 0, PointField.FLOAT32, 1),
+        PointField('y', 4, PointField.FLOAT32, 1),
+        PointField('z', 8, PointField.FLOAT32, 1)]
+    msg.is_bigendian = False
+    msg.point_step = 12
+    msg.row_step = 12*points.shape[0]
+    msg.is_dense = int(np.isfinite(points).all())
+    msg.data = np.asarray(points, np.float32).tostring()
+    return msg
+
+def matrixToPose(matrix):
+    """Converts a Matrix  to a Pose message by extracting the translation and
     rotation specified by the Matrix.
 
     Parameters:
     matrix: a Matrix4 (euclid.py)
     """
     pos = Pose()
-    pos.position.x = matrix.d
-    pos.position.y = matrix.h
-    pos.position.z = matrix.l
+    T = transformations.translation_from_matrix(matrix)
+    pos.position.x = T[0]
+    pos.position.y = T[1]
+    pos.position.z = T[2]
     
-    q = matrix.get_quaternion()
-    pos.orientation.x = q.x
-    pos.orientation.y = q.y
-    pos.orientation.z = q.z
-    pos.orientation.w = q.w
+    q = transformations.quaternion_from_matrix(matrix)
+    pos.orientation.x = q[0]
+    pos.orientation.y = q[1]
+    pos.orientation.z = q[2]
+    pos.orientation.w = q[3]
 
     return pos
 
-def poseTomatrix4(pose):
+def poseTomatrix(pose):
     """Converts a Pose message to a matrix 4.
     """
     assert isinstance(pose, Pose)
     
-    mat = euclid.Matrix4()
-    mat.translate(pose.position.x,
-                      pose.position.y,
-                      pose.position.z, )        
-    rot = euclid.Quaternion(pose.orientation.w,
+    T = ((pose.position.x,
+                                              pose.position.y,
+                                              pose.position.z, )
+                                             )
+    R = transformations.euler_from_quaternion((
                             pose.orientation.x,
                             pose.orientation.y,
-                            pose.orientation.z, )
-    mat *= rot.get_matrix()
+                            pose.orientation.z,
+                            pose.orientation.w)
+                 )
     
-    return mat
+    return transformations.compose_matrix(translate=T,
+                                          angles=R)
 
-def makeGripperMarker(pose, angle=0.541, color=None, scale=1.0):
+def makeGripperMarker(pose, angle=0.0, color=None, scale=1.0):
     """
     Creates an MarkerArray with the PR2 gripper shape. 
     Parameters:
@@ -55,20 +115,25 @@ def makeGripperMarker(pose, angle=0.541, color=None, scale=1.0):
     The new gripper MarkerArray
     """
     assert isinstance(pose, PoseStamped)
-    pose_T = poseTomatrix4(pose.pose)
+    pose_T = poseTomatrix(pose.pose)
 
     gripper_marker = MarkerArray()
 
-    T1 = euclid.Matrix4()
-    T2 = euclid.Matrix4()
-
-    T1.translate(0.07691, 0.01, 0.)
-    T1.rotate_axis(angle, euclid.Vector3(0,0,1))
-    T2.translate(0.09137, 0.00495, 0.)
-    T1.rotate_axis(-angle, euclid.Vector3(0,0,1))
+    
+    T1 = transformations.translation_matrix((0.07691, 0.01, 0.))
+    T1 = T1.dot(transformations.rotation_matrix(angle, (0, 0, 1)))
+    T2 = transformations.translation_matrix((0.09137, 0.00495, 0.))
+    T2 = T2.dot(transformations.rotation_matrix(angle, (0, 0, 1)))
+    
+    #T1 = euclid.Matrix4()
+    #T2 = euclid.Matrix4()
+    #T1.translate(0.07691, 0.01, 0.)
+    #T1.rotate_axis(angle, euclid.Vector3(0,0,1))
+    #T2.translate(0.09137, 0.00495, 0.)
+    #T1.rotate_axis(-angle, euclid.Vector3(0,0,1))
 
     T_proximal = T1.copy()
-    T_distal = T1 * T2
+    T_distal = T1.dot(T2)
 
     mesh = Marker()
     mesh.header.frame_id = pose.header.frame_id
@@ -87,36 +152,40 @@ def makeGripperMarker(pose, angle=0.541, color=None, scale=1.0):
         mesh.mesh_use_embedded_materials = True
 
     mesh.mesh_resource = "package://pr2_description/meshes/gripper_v0/gripper_palm.dae"
-    mesh.pose = matrix4ToPose(pose_T)
-    #mesh.pose.orientation.w = 1
+    mesh.pose = matrixToPose(pose_T)
     gripper_marker.markers.append(copy.deepcopy(mesh))
     
     mesh.mesh_resource = "package://pr2_description/meshes/gripper_v0/l_finger.dae"
-    mesh.pose = matrix4ToPose(pose_T * T_proximal)
+    mesh.pose = matrixToPose(pose_T.dot(T_proximal))
     gripper_marker.markers.append(copy.deepcopy(mesh))
 
     mesh.mesh_resource = "package://pr2_description/meshes/gripper_v0/l_finger_tip.dae"
-    mesh.pose = matrix4ToPose(pose_T * T_distal)
+    mesh.pose = matrixToPose(pose_T.dot(T_distal))
     gripper_marker.markers.append(copy.deepcopy(mesh))
 
-    T1 = euclid.Matrix4()
-    T2 = euclid.Matrix4()
-
-    T1.translate(0.07691, -0.01, 0.)
-    T1.rotate_axis(np.pi, euclid.Vector3(1,0,0))
-    T1.rotate_axis(angle, euclid.Vector3(0,0,1))
-    T2.translate(0.09137, 0.00495, 0.)
-    T1.rotate_axis(-angle, euclid.Vector3(0,0,1))
+    #T1 = euclid.Matrix4()
+    #T2 = euclid.Matrix4()
+    #T1.translate(0.07691, -0.01, 0.)
+    #T1.rotate_axis(np.pi, euclid.Vector3(1,0,0))
+    #T1.rotate_axis(angle, euclid.Vector3(0,0,1))
+    #T2.translate(0.09137, 0.00495, 0.)
+    #T1.rotate_axis(-angle, euclid.Vector3(0,0,1))
+    
+    T1 = transformations.translation_matrix((0.07691, -0.01, 0.))
+    T1 = T1.dot(transformations.rotation_matrix(np.pi, (1, 0, 0)))
+    T1 = T1.dot(transformations.rotation_matrix(angle, (0, 0, 1)))
+    T2 = transformations.translation_matrix((0.09137, 0.00495, 0.))
+    T2 = T2.dot(transformations.rotation_matrix(-angle, (0, 0, 1)))    
 
     T_proximal = T1.copy()
-    T_distal = T1 * T2
+    T_distal = T1.dot(T2)
 
     mesh.mesh_resource = "package://pr2_description/meshes/gripper_v0/l_finger.dae"
-    mesh.pose = matrix4ToPose(pose_T * T_proximal)
+    mesh.pose = matrixToPose(pose_T.dot(T_proximal))
     gripper_marker.markers.append(copy.deepcopy(mesh))
 
     mesh.mesh_resource = "package://pr2_description/meshes/gripper_v0/l_finger_tip.dae"
-    mesh.pose = matrix4ToPose(pose_T * T_distal)
+    mesh.pose = matrixToPose(pose_T .dot(T_distal))
     gripper_marker.markers.append(copy.deepcopy(mesh))
 
     for i, marker in enumerate(gripper_marker.markers):
