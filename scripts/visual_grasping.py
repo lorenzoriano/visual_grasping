11d46dfad2b5ab5dee544cd2d664316f416ff90d
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-03-09 11:42:42 -0800

    fininshig script

--- scripts/visual_grasping.py
+++ scripts/visual_grasping.py
@@ -2,23 +2,27 @@
 import roslib
 roslib.load_manifest("visual_grasping")
 import rospy
-import visual_grasper
+import visual_grasping.visual_grasper
 import openrave_bridge.env_operations
 from sensor_msgs.msg import PointCloud2, PointCloud
 from geometry_msgs.msg import Pose, PoseArray, PoseStamped
 import tabletop_actions
 import sys
 
-robot = openrave_bridge.pr2model.PR2Robot()
-detector = tabletop_actions.object_detector.GenericDetector()
-detector.detect();
-if detector.last_detection_msg is None:
-    rospy.logerr("No table detected, tilt the head down!")
-    sys.exit(0)
-
-openrave_bridge.env_operations.add_table(
-    detector.last_detection_msg, robot.env)
-vg = visual_grasper.VisualGrasper(robot)
-vg.work_on_bolt_pointcloud()
+if __name__ == "__main__":
+    rospy.init_node("visual_grasping")
+    rospy.loginfo("Creating the robot")
+    robot = openrave_bridge.pr2model.PR2Robot()
+    rospy.loginfo("Finding the table")
+    detector = tabletop_actions.object_detector.GenericDetector()
+    
+    detector.detect();
+    if detector.last_detection_msg is None:
+        rospy.logerr("No table detected, the robot might hit the table!")
+    else:    
+        openrave_bridge.env_operations.add_table(
+            detector.last_detection_msg, robot.env)
+    vg = visual_grasping.visual_grasper.VisualGrasper(robot)
+    vg.work_on_bolt_pointcloud()
     
 
